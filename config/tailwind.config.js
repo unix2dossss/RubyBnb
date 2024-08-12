1dfe243c0276f3c1ae9eb244b7ff961d02d820c6
Vandan Bhatt <vandan.ubuntu@gmail.com>
2024-08-13 03:16:02 +1200

    Create Header

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
@@ -11,6 +14,13 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        ...colors,
+        primary: {
+            DEFAULT: '#ff5a5f',
+            dark: '#ff385c',
+        }
       },
     },
   },
